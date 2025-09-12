0895852ed6b66d4708f47cd9fdd395b4bd0d3004
Hanbin Yim <hyim@academic.rrc.ca>
2025-09-12 10:32:21 -0500

    add import and def

---
+++ assignment_1_planning.py
@@ -0,0 +1,29 @@
+# understanding the data
+# how do we handle hyphenated words?
+# treat them as multiple words
+# we do not need to handle numbers
+# same word with different capitalization is considered the same
+
+# read data from the supplied file (input.txt)
+
+# process the data (get it ready for ... ???)
+
+# normalize (lower())
+# tokenize (split())
+# handle punctuation
+# handle extra whitespace (strip())
+
+# collect/accumulate stats (think about data structures)
+# count the words (total and unique)
+# count the chars (with and without whitespace)
+# calculate the average word length
+# find the most common word(s) dict()
+
+# output the stats in the prescribed format
+# output to console
+# output to file
+# !! no repeats!!
+
+
+
+
